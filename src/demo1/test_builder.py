43272d03b34736c8760d05d8d65fe823379e2258
Minh Mốc Meo <minh@xuvi.vn>
2024-12-05 09:19:12 +0700

    WIP

--- src/demo1/test_builder.py
+++ src/demo1/test_builder.py
@@ -1,15 +1,24 @@
 import argparse
+import os
 import subprocess
 
 
 def main():
     print("run main")
     get_input = argparse.ArgumentParser()
-    get_input.add_argument("--target", help="target project to build")
+    get_input.add_argument("--target", default="all", help="target project to build")
 
     args = get_input.parse_args()
 
-    subprocess.run(["make", args.target], cwd="/Users/moc/Projects/python_automation_for_embedded_engineer/src/demo1")
+    current_environment = os.environ.copy()
+    # current_environment["PATH"] = "C:/cygwin64/bin:" + current_environment["PATH"]
+    subprocess.run(
+        ["make", args.target],
+        cwd="/Users/moc/Projects/python_automation_for_embedded_engineer/src/demo1",
+        env=current_environment,
+        shell=True,
+        check=True,
+    )
 
 
 if __name__ == "__main__":
